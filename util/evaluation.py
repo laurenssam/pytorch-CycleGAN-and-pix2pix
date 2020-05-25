bb019987f0bbddd117670a50db1582dbf2e1b8a1
Laurens Samson <laurenssamson@hotmail.com>
2020-05-25 18:34:03 +0200

    evaluation fixed

--- util/evaluation.py
+++ util/evaluation.py
@@ -2,35 +2,53 @@ import torch
 import numpy as np
 from torchvision.datasets import Cityscapes
 
-def _fast_hist(label_true, label_pred, n_class):
-    mask = (label_true >= 0) & (label_true < n_class)
-    hist = np.bincount(
-        n_class * label_true[mask].astype(int) + label_pred[mask],
-        minlength=n_class ** 2,
-    ).reshape(n_class, n_class)
-    return hist
+# Adapted from score written by wkentaro
+# https://github.com/wkentaro/pytorch-fcn/blob/master/torchfcn/utils.py
+
+import numpy as np
 
 
-def scores(label_trues, label_preds, n_class):
-    hist = np.zeros((n_class, n_class))
-    for lt, lp in zip(label_trues, label_preds):
-        hist += _fast_hist(lt.flatten(), lp.flatten(), n_class)
-    acc = np.diag(hist).sum() / hist.sum()
-    acc_cls = np.diag(hist) / hist.sum(axis=1)
-    acc_cls = np.nanmean(acc_cls)
-    iu = np.diag(hist) / (hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist))
-    valid = hist.sum(axis=1) > 0  # added
-    mean_iu = np.nanmean(iu[valid])
-    freq = hist.sum(axis=1) / hist.sum()
-    fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
-    cls_iu = dict(zip(range(n_class), iu))
-    return {
-        "Pixel Accuracy": acc,
-        "Mean Accuracy": acc_cls,
-        "Frequency Weighted IoU": fwavacc,
-        "Mean IoU": mean_iu,
-        "Class IoU": cls_iu,
-    }
+class runningScore(object):
+    def __init__(self, n_classes):
+        self.n_classes = n_classes
+        self.confusion_matrix = np.zeros((n_classes, n_classes))
+
+    def _fast_hist(self, label_true, label_pred, n_class):
+        mask = (label_true >= 0) & (label_true < n_class)
+        hist = np.bincount(
+            n_class * label_true[mask].astype(int) + label_pred[mask], minlength=n_class ** 2
+        ).reshape(n_class, n_class)
+        return hist
+
+    def update(self, label_trues, label_preds):
+        for lt, lp in zip(label_trues, label_preds):
+            self.confusion_matrix += self._fast_hist(lt.flatten(), lp.flatten(), self.n_classes)
+
+    def get_scores(self):
+        """Returns accuracy score evaluation result.
+            - overall accuracy
+            - mean accuracy
+            - mean IU
+            - fwavacc
+        """
+        hist = self.confusion_matrix
+        acc = np.diag(hist).sum() / hist.sum()
+        acc_cls = np.diag(hist) / hist.sum(axis=1)
+        acc_cls = np.nanmean(acc_cls)
+        iu = np.diag(hist) / (hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist))
+        mean_iu = np.nanmean(iu)
+        freq = hist.sum(axis=1) / hist.sum()
+        fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
+        cls_iu = dict(zip(range(self.n_classes), iu))
+
+        return {'Pixel Accuracy' : acc,
+                'Frequency Weighted IoU': fwavacc,
+                'Mean IoU': mean_iu,
+                'Class IoU': cls_iu}
+
+
+    def reset(self):
+        self.confusion_matrix = np.zeros((self.n_classes, self.n_classes))
 
 
 def calculate_loss(predictions, labels, loss_function):
@@ -39,10 +57,9 @@ def calculate_loss(predictions, labels, loss_function):
     return average_loss.item()
 
 
-def run_inference(data_loader, model, print_freq=3):
+def run_inference(data_loader, model, print_freq=50):
     model.netG.eval()
-    predictions = []
-    labels = []
+    scores = runningScore(model.num_classes)
     loss_function = model.criterion
     losses = []
     for i, (input_image, label) in enumerate(data_loader):
@@ -50,17 +67,16 @@ def run_inference(data_loader, model, print_freq=3):
         prediction = get_prediction(input_image, model)
         losses.append(calculate_loss(prediction, label, loss_function))
         prediction_argmax = torch.argmax(prediction.cpu(), dim=1)
-        predictions.append(prediction_argmax)
-        labels.append(label.cpu())
+        prediction_argmax[label == 255] = 255
+        scores.update(label.numpy(), prediction_argmax.numpy())
+        if i == 10:
+            break
         assert label.shape != prediction_argmax, \
             f"shape of label and prediction are not the same (label/pred): {label.shape}/{prediction_argmax.shape}"
         if i > 0 and i % print_freq == 0:
             print(f"Finished {i}/{len(data_loader)}")
     model.netG.train()
-    predictions = torch.cat(predictions, dim=0)
-    labels= torch.cat(labels, dim=0)
-    predictions[labels == 255] =255
-    return predictions.numpy(), labels.numpy(), np.mean(losses)
+    return scores, np.mean(losses)
 
 
 def get_prediction(input_image, model):
@@ -71,13 +87,13 @@ def get_prediction(input_image, model):
 
 def evaluate(train_loader, val_loader, model):
     train_id_to_name = {cls[2]:cls[0] for cls in Cityscapes.classes if not cls[6]}
-    train_predictions, train_labels, train_loss = run_inference(train_loader, model)
-    train_stats = scores(train_predictions, train_labels, model.num_classes)
-    del train_predictions, train_labels
 
-    val_predictions, val_labels, val_loss = run_inference(val_loader, model)
-    val_stats = scores(val_predictions, val_labels, model.num_classes)
-    del val_predictions, val_labels
+    train_scores, train_loss = run_inference(train_loader, model)
+    train_stats = train_scores.get_scores()
+
+
+    val_scores, val_loss = run_inference(val_loader, model)
+    val_stats = train_scores.get_scores()
 
     model.store_losses((train_loss, val_loss))
     model.store_ious((train_stats['Mean IoU'], val_stats['Mean IoU']))
